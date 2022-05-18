408468c88383324ac8245beb9deebfc0936cebbf
AlexKomanov <akomanov88@gmail.com>
2022-05-18 23:01:25 +0300

    Adding hooks to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BoardComponent from './components/BoardComponent';
 import './App.css';
+import { Board } from './models/Board';
 
 
 const  App = () => {
+
+    const [board, setBoard] = useState(new Board());
+
+    useEffect(() => {
+      restart();
+    }, [])
+
+    const restart = () => {
+      const newBoard = new Board();
+      newBoard.initCells();
+      setBoard(newBoard);
+    }
+
+
+
   return (
     <div className="app">
-     <BoardComponent/>
+     <BoardComponent 
+     board={board}
+     setBoard={setBoard}
+     />
     </div>
   );
 }
